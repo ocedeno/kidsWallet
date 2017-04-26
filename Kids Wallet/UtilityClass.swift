13271debd4adccff066cd2c99f7f4b7351973433
Octavio Cedeno <ocedeno@users.noreply.github.com>
2017-04-26 09:58:27 -0400

    docs: Add utilityClass to manage general methods and extensions

---
+++ Kids Wallet/UtilityClass.swift
@@ -0,0 +1,17 @@
+//
+//  UtilityClass.swift
+//  Kids Wallet
+//
+//  Created by Octavio Cedeno on 4/26/17.
+//  Copyright © 2017 Cedeno Enterprises, Inc. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage
+{
+    class func profilePlaceholder() -> UIImage
+    {
+        return UIImage(named: "profilePlaceholder")!
+    }
+}
